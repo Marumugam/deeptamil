e4e0662db8f7b02f72769919e6d45f34cf716344
Suriyadeepan Ram <suriyadeepan@acm.org>
2015-10-29 12:02:17 +0530

    fixed the outdated preprocessing module

--- src/preprocess/_main.py
+++ src/preprocess/_main.py
@@ -3,6 +3,7 @@
 import cv2
 import numpy as np
 import sys
+import os
 
 SZ = 120
 affine_flags = cv2.WARP_INVERSE_MAP|cv2.INTER_LINEAR
@@ -59,27 +60,38 @@ def writeToFile(thresh,centered,deskewed):
 	cv2.imwrite(output_path + "/" + file_name + ".jpg", centered)
 
 
-def display(thresh,centered,deskewed):
-	cv2.imshow('original',thresh)
-	cv2.imshow('centered',centered)
-	cv2.imshow('deskewed',deskewed)
+def display(im):
+	cv2.imshow('finale',im)
 	cv2.waitKey(0)
+
+
+def dilate(centered):
+	kernel = np.ones((5,5),np.uint8)
+	dilation = cv2.dilate(centered,kernel,iterations = 1)
+	return dilation
 
 if __name__ == '__main__':
 	file_path = sys.argv[1]
-	file_name = sys.argv[2]
-	output_path = sys.argv[3]
+	output_path = sys.argv[2]
 	src = cv2.imread(file_path,0)
+	print(sys.argv[0] + ": operating over " + file_path)
+
 	src = cv2.resize(src,(120,120), interpolation = cv2.INTER_CUBIC)
 
 	thresh = morph(src).copy()
-	deskewed = deskew(thresh)
+	dilated = dilate(thresh)
+	deskewed = deskew(dilated)
 	deskewed_copy = deskewed.copy()
 	roi = roi_op(deskewed_copy,deskewed).copy()
 	centered = center(roi)
+	#display(centered)
 
+	centered = cv2.resize(centered,(30,30), interpolation = cv2.INTER_CUBIC)
 	# write to file
-	writeToFile(thresh,centered,deskewed)
+	file_name, file_ext = os.path.splitext(os.path.basename(file_path))
+
+	#cv2.imwrite(output_path + "/" + file_name + "_orig" + ".jpg", src)
+	cv2.imwrite(output_path + "/" + file_name + ".png", centered)
 
 	# display
 	#display(thresh,centered,deskewed)
